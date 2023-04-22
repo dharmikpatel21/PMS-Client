3cff0154bd21fffef73578984216b0c55a103d0d
Jay2182 <jaykanjia11@gmail.com>
2023-04-22 09:48:08 +0530

    loading screen done

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import ReactLoading from "react-loading";
 import "../css/loading.css";
 
 const Loading = () => {
 	return (
-		<div className="loading-container flex justify-center items-center">
-			<h1>Loading...</h1>
+		<div className="loading-container">
+			<ReactLoading
+				type="spinningBubbles"
+				color="var(--primary-color)"
+				height={100}
+				width={100}
+			/>
+			<h4>Loading...</h4>
 		</div>
 	);
 };
